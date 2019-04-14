dcbd3b108803aa5bbf0401192622dee58e7a7740
mrxuying <756880285@qq.com>
2019-04-14 22:34:29 +0800

    second commit

--- sign/views.py
+++ sign/views.py
@@ -13,11 +13,13 @@ def login_action(request):
         password = request.POST.get('password','')
         if username == 'admin' and password == 'admin123':
             response = HttpResponseRedirect('/event_manage/')
-            response.set_cookie('user',username,3600)   #添加浏览器cookie,3600是cookie存在的时间，单位是秒
+            #response.set_cookie('user',username,3600)   #添加浏览器cookie,3600是有效时间，单位s"user"是cookie的名称；
+            request.session['user'] = username
             return response
         else:
             return render(request,'index.html',{'error':'username or password error!'})
 
 def event_manage(request):
-    username = request.COOKIES.get('user','')       #读取浏览器cookie
+    #username = request.COOKIES.get('user','')       #读取浏览器cookie
+    username = request.session.get('user','')   #读取浏览器session
     return render(request,"event_manage.html",{"user":username})
